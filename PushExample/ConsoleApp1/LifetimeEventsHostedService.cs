d40ff507eec775115bc9d7cb1d858d9a7bc33d04
Jin Wang <jin.wang@bcrdteam.onmicrosoft.com>
2021-07-26 03:43:32 +0000

    sample

---
+++ PushExample/ConsoleApp1/LifetimeEventsHostedService.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Prometheus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   internal  class LifetimeEventsHostedService: IHostedService
+    {
+        private readonly ILogger _logger;
+
+
+        private Gauge UsersLoggedIn = null;
+
+        private  CollectorRegistry _reg = new CollectorRegistry();
+
+
+
+        public LifetimeEventsHostedService(
+        ILogger<LifetimeEventsHostedService> logger,
+        IHostApplicationLifetime appLifetime)
+        {
+            _logger = logger;
+
+            _reg.SetStaticLabels(new Dictionary<string, string>
+{
+  // Labels applied to all metrics in the registry.
+  { "environment", "testing" }
+});
+
+
+
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+          var factory =   Metrics.WithCustomRegistry(_reg);
+
+            UsersLoggedIn = factory.CreateGauge("myapp_users_logged_in", "Number of active user sessions",
+            new GaugeConfiguration
+            {
+            SuppressInitialValue = true
+            });
+
+            // throw new NotImplementedException();
+            var pusher = new MetricPusher(new MetricPusherOptions
+            {
+                Endpoint = "http://prom-gateway.chinaeast2.cloudapp.chinacloudapi.cn:8000/metrics",
+                Job = "some_job1",
+                Instance = "Instance1",
+                AdditionalLabels = new[] { new Tuple<string, string>("t1", "t2")},
+                Registry = _reg,
+                OnError = ex =>
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            });
+
+            pusher.Start();
+
+
+            UsersLoggedIn.Set(1);
+            UsersLoggedIn.Publish();
+            return Task.Delay(0);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.Delay(0);
+        }
+    }
+}
